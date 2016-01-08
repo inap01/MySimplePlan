600fe3671382ba2c4f4b22247f1e9fccdcc4f60f
Sebastian Aretz <seba.aretz@hotmail.de>
2016-01-08 12:38:47 +0100

    Tages Kalorienübersicht Status post

--- app/components/home/controller.js
+++ app/components/home/controller.js
@@ -7,14 +7,36 @@
 
     HomeController.$inject = ['$scope', '$http'];
     function HomeController($scope, $http) {
-        $scope.test = "test";
 
+
+
+        $scope.increment = function(){
+
+            $scope.calorie_now = $scope.calorie_now + 100;
+            $scope.calorie_percent = ($scope.calorie_now * 100) / $scope.calorie_max;
+            $scope.calorie_open = $scope.calorie_max - $scope.calorie_now;
+
+            if($scope.calorie_open < 0)
+            {
+                $scope.calorie_style = {color:'red'}
+            }
+        }
+
+        $('[data-toggle="tooltip"]').tooltip()
         init();
 
         function init() {
+
+            $scope.calorie_style = {};
+            $scope.calorie_max = 3800;
+            $scope.calorie_now = 1200;
+            $scope.calorie_open = $scope.calorie_max - $scope.calorie_now;
+            $scope.calorie_percent = ($scope.calorie_now * 100) / $scope.calorie_max;
+
         }
     }
 
 })();
+
 
 
