5a6af0ecbed2c61fdd5dd02d138d6bd017c6d1ff
inap01 <marius8hartmann@gmail.com>
2016-01-13 21:16:28 +0100

    Splitted Diary Breakfast from controller

--- app/components/diary/controller.js
+++ app/components/diary/controller.js
@@ -7,6 +7,11 @@
 
     DiaryController.$inject = ['$scope', '$http'];
     function DiaryController($scope, $http) {
+        $scope.breakfast = [
+            {name:'Haferflocken', value: '200g', calories: 100},
+            {name:'Milch', value: '500ml', calories: 120},
+            {name:'Banane', value: '1 Stück', calories: 96}
+        ];
 
         init();
 
